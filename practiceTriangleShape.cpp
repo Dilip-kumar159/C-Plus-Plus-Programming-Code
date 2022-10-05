10fa8283cf941589fdbba61b50332a16d59d59ba
Dilip <dilip45377@gmail.com>
2022-10-05 21:25:33 +0530

    Added one more apprach

--- practiceTriangleShape.cpp
+++ practiceTriangleShape.cpp
@@ -7,29 +7,43 @@ int main(){
    int num;
    cout<<"Enter the Number : "<<endl;
     cin>>num;
-    int row = 1;
+    // int row = 1;
 
-    while(row <= num){
-        int space = num - row;
-        while(space){
+    // while(row <= num){
+    //     int space = num - row;
+    //     while(space){
+    //         cout<<" ";
+    //         space--;
+    //     }
+
+    //     int col = 1;
+    //     while(col<=row){
+    //         cout<<"*";
+    //         col++;
+    //     }
+
+    //     int start = row - 1;
+    //     while(start){
+    //         cout<<"*";
+    //         start--;
+    //     }
+
+    //     cout<<endl;
+    //     row++;
+    // }
+
+
+    for(int i=0; i<=num; i++){
+        
+        for(int j=0; j< num-i; j++){
             cout<<" ";
-            space--;
         }
 
-        int col = 1;
-        while(col<=row){
-            cout<<"*";
-            col++;
+        for(int j=1; j<=i; j++){
+            cout<<"* ";
         }
-
-        int start = row - 1;
-        while(start){
-            cout<<"*";
-            start--;
-        }
-
+        
         cout<<endl;
-        row++;
     }
     return 0;
 }
